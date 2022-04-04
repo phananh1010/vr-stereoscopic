d7916a689ab57ea31db9c10a8a60a04ae9e013d6
Anh Nguyen <phananh1010@yahoo.com>
2022-04-04 00:04:35 -0400

    data loader ready, coding the network

--- dataset.py
+++ dataset.py
@@ -1,25 +1,48 @@
 import torch as tc
 import torchvision as tv
-
+from PIL import Image
 import numpy as np
 import glob
 
 class StereoReader(tc.utils.data.Dataset):
     
-    def __init__(self, ):
-        
+    def __init__(self, filepath, transform):
+        #TODO: read all file in the given folder
+        self.filepath_dict, self.vid_filepath_list, self.frame_filepath_list = self.extract_file_hierachy(filepath)
+        self.transform = transform
+        return
     
     def __len__(self):
-        return len(self.tilename_list)
+        return len(self.frame_filepath_list)/2
     
     def __getitem__(self, idx):
-        self.check_internal_consistency()
-        vid_idx, seg_idx, tile_idx = self.get_indices(idx)
-        tilepath = self.dat[vid_idx][seg_idx][tile_idx]
-        result = {self.KEY_FILEPATH: tilepath, 
-                  self.KEY_IDX: idx, 
-                  self.KEY_VIDIDX: vid_idx, 
-                  self.KEY_SEGIDX: seg_idx,
-                  self.KEY_TILEIDX: tile_idx}
-        result = {**result, **self.parse_info(tilepath)}
-        return result
+        frame_filepath_left, frame_filepath_right = self.frame_filepath_list[idx*2], self.frame_filepath_list[idx*2+1], 
+        img_left, img_right = self.read_image(frame_filepath)
+        return img_left, img_right
+    
+    #### FUNCTION to initialize file structure from dataset directory
+    def explore_directory(self, filepath):
+        #TODO: given a filepath: return list of file/dir which is direct child of the input
+        filepath_list = glob.glob(filepath + '/*')
+        file_list = [item.split('/')[-1] for item in filepath_list]
+        return filepath_list, file_list
+    
+    def extract_file_hierachy(self, filepath):
+        #TODO: for the root filepath, extract list of videos, and the frames for each video
+        #INPUT: root dir path
+        #OUTPUT: python directory storing the result with key is the filename & vidname, list of all vid, list of all file
+        result = {}
+        frame_filepath_list_total = []
+        vid_filepath_list, vid_filename_list = self.explore_directory(filepath)
+        for idx, vid_filepath in enumerate(vid_filepath_list):
+            vid_filename = vid_filename_list[idx]
+            frame_filepath_list, frame_filename_list = self.explore_directory(vid_filepath)
+            frame_filename_list = sorted(frame_filename_list) #sorted, to get left right easily
+            result[vid_filename] = frame_filepath_list
+            frame_filepath_list_total += frame_filepath_list
+        return result, vid_filepath_list, frame_filepath_list_total
+    
+    ##### Functions to read & process image
+    def read_image(self, frame_filepath):
+        return self.transform(Image.open(frame_filepath))
+    
---
+++ networks.py
@@ -0,0 +1,9 @@
+import torch as tc
+import numpy as np
+
+class Generator(tc.nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        return
+    
+    #TODO: create a simple 
